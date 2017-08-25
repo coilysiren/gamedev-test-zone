78da1b35252d880e024bc869ea2a2ccbbe5a28f0
Lynn Cyrin <firemagelynn@gmail.com>
2017-08-25 16:45:46 -0700

    WIP frontend

--- client/assets/js/game.js
+++ client/assets/js/game.js
@@ -15,14 +15,9 @@ var game = new Phaser.Game(
 );
 
 function preload() {
-    game.load.image('sky', 'assets/sky.png');
-    game.load.image('ground', 'assets/platform.png');
-    game.load.image('star', 'assets/star.png');
-    game.load.spritesheet('dude', 'assets/dude.png', 32, 48);
 }
 
 function create() {
-    game.add.sprite(0, 0, 'star');
 }
 
 function update() {
