bff75f3d248fc37eb823426594f4998fc3eb044a
Lynn Cyrin <firemagelynn@gmail.com>
2017-08-25 11:04:02 -0700

    webpack compiling successfully

--- src/webpack.config.js
+++ src/webpack.config.js
@@ -1,16 +1,15 @@
-const phaser = './node_modules/phaser-ce/build/custom/phaser-split.js'
-const pixi = './node_modules/phaser-ce/build/custom/pixi.js'
-const p2 = './node_modules/phaser-ce/build/custom/p2.js'
+var phaser = __dirname + '/../node_modules/phaser-ce/build/custom/phaser-split.js'
+var pixi = __dirname + '/../node_modules/phaser-ce/build/custom/pixi.js'
+var p2 = __dirname + '/../node_modules/phaser-ce/build/custom/p2.js'
 
 module.exports = {
-  entry: './client/game.js',
   entry: {
-    app: ['./client/game.js'],
-    vendor: ['pixi', 'p2', 'phaser']
+    vendor: ['pixi', 'p2', 'phaser'],
+    main: ['./client/game.js']
   },
   output: {
     filename: "[name].js",
-    path: __dirname + '/dist'
+    path: __dirname + '/../dist'
   },
   watch: true,
   node: {
