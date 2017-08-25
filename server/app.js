546da64cd96e898bc39345ac994708e848678bd2
Lynn Cyrin <firemagelynn@gmail.com>
2017-08-25 14:43:20 -0700

    :innocent:

--- server/app.js
+++ server/app.js
@@ -3,9 +3,9 @@ var app = express()
 var DEFAULT_PORT = 8080
 
 app.set('view engine', 'ejs')
-app.set("port", process.env.PORT || DEFAULT_PORT)
+app.set('port', process.env.PORT || DEFAULT_PORT)
 
-if (process.env.NODE_ENV !== "production") {
+if (process.env.NODE_ENV !== 'production') {
     var webpack = require('webpack')
     var webpackDevMiddleware = require('webpack-dev-middleware')
     var webpackHotMiddleware = require('webpack-hot-middleware')
@@ -21,5 +21,5 @@ app.get('/', (req, res) => {
 
 app.use(express.static(__dirname + '/../dist'))
 
-app.listen(app.get("port"))
-console.log('listening on port '+ app.get("port"))
+app.listen(app.get('port'))
+console.log('listening on port '+ app.get('port'))
